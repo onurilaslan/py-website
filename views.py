0bcd751cc98ab1cdad3aae96866db3c0ef3068d1
onuri <onurilaslann@gmail.com>
2023-03-20 02:05:50 +0300

    first commit

---
+++ views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, "views")
+
+
+@views.route("/")
+def home():
+    return render_template("index.html", name="onur", )
+
+
+# @views.route("/profile/<username>")
+# def profile(username):
+#     return render_template("index.html", names=username)

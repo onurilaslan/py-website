2fff83e9a1add2f7b85085dcb2a987dc66c419a5
onuri <onurilaslann@gmail.com>
2023-03-20 02:24:55 +0300

    javascript kodları üzerinde hatalar fixlendi

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
@@ -8,6 +8,25 @@ def home():
     return render_template("index.html", name="onur", )
 
 
-@views.route("/profile/<username>")
-def profile(username):
-    return render_template("index.html", names=username)
+@views.route("/profile")
+def profile():
+    args = request.args
+    name = args.get('name')
+    return render_template("index.html", name=name)
+
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'tim', 'adminLevel': 5})
+
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+
+@views.route("/go-to-home")
+def go_to_home():
+    # return redirect(url_for("views.get_json"))
+    return redirect(url_for("views.home"))
